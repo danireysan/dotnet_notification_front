2f679735e9db6dbfeb3aca774db26ceff620fe88
Daniel Reynoso Sanchez <danireysan@gmail.com>
2026-02-21 18:24:00 -0600

    background handler

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,10 @@ import 'package:dotnet_notification_front/core/instances/core_instances.dart';
 import 'package:dotnet_notification_front/features/notifications/instances/notifications_instances.dart';
 import 'package:dotnet_notification_front/main_config/main_app.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'core/di/inject_all_dependencies.dart';
 import 'firebase_options.dart';
-
-@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +15,6 @@ Future<void> main() async {
   // Notifications setup
   await localNotificationRepository.initialize();
   notificationService.initialize();
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   apiClient.healthCheck();
   runApp(const MainApp());
