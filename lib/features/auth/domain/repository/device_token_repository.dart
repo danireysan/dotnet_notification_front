d86eb0749b4ab8f05189ca12e517e2eae3060eff
Daniel Reynoso Sanchez <danireysan@gmail.com>
2026-02-21 18:28:31 -0600

    deleted unused repositories

--- lib/features/auth/domain/repository/device_token_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dotnet_notification_front/core/domain/error/failure.dart';
-import 'package:dotnet_notification_front/features/auth/domain/entities/device_token_entity.dart';
-import 'package:fpdart/fpdart.dart';
-
-abstract class DeviceTokenRepository {
-  Future<Either<Failure, Unit>> saveDeviceToken(String token);
-  Future<Either<Failure, DeviceTokenEntity>> getDeviceToken();
-}
